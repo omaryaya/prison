f521685ac6d344ca5256bb7bd3fdac83143d0dcf
omaryaya <omar.yaya.work@gmail.com>
2023-08-26 21:35:49 +0300

    Initial commit

---
+++ src/test/java/com/omaryaya/prison/PrisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omaryaya.prison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
